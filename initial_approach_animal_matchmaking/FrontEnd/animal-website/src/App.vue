fac183bfe675dc344229dafe94ae2bf4418851fd
RedMarshmellow <ahmedhaniaminmohamed@gmail.com>
2022-02-20 19:57:16 +0300

    Vuejs Landing Page Prototype

--- initial_approach_animal_matchmaking/FrontEnd/animal-website/src/App.vue
+++ initial_approach_animal_matchmaking/FrontEnd/animal-website/src/App.vue
@@ -1,31 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <LoginPage/>
+  </div>
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import LoginPage from "@/views/LoginPage";
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: "App",
+  components: {
+    LoginPage
   }
 }
+</script>
+
+<style lang="scss">
+#app {
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+  align-content: center;
+  align-self: center;
+  height: 600px;
+  width: 800px;
+}
+
+body {
+  background-image: url("assets/login-background.jpg");
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-self: center;
+  height: 100vh;
+}
+
+@import '~bootstrap';
 </style>
---
+++ initial_approach_animal_matchmaking/FrontEnd/animal-website/src/views/LoginPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <h1 class="greet">Welcome!</h1>
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input id="exampleInputEmail1" aria-describedby="emailHelp" class="form-control" placeholder="Enter email"
+               type="email">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input id="exampleInputPassword1" class="form-control" placeholder="Password" type="password">
+      </div>
+      <div class="text-center mt-4">
+        <button class="btn-lg btn-primary " type="submit">Submit</button>
+      </div>
+    </form>
+    <div class="text-center mt-4"><a href="/register">
+      <button class="btn-lg btn-secondary " type="submit">New User?</button>
+    </a></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginPage"
+}
+</script>
+
+<style lang="scss" scoped>
+.btn-primary {
+  background-color: #DA88DC;
+}
+
+.btn-secondary {
+  background-color: #68B7F0;
+  margin-top: 2em;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.greet {
+  margin-bottom: 2em;
+}
+</style>
