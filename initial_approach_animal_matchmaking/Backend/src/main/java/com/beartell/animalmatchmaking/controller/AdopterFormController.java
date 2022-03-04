3a0accbace0e7cd26acbc6c4c65210f3850a8cbe
Hevra Petekkaya <hevrapetekkaya01@gmail.com>
2022-03-04 17:19:36 +0200

    refactor the adopterForm Controller

--- initial_approach_animal_matchmaking/Backend/src/main/java/com/beartell/animalmatchmaking/controller/AdopterFormController.java
+++ initial_approach_animal_matchmaking/Backend/src/main/java/com/beartell/animalmatchmaking/controller/AdopterFormController.java
@@ -4,18 +4,25 @@ import com.beartell.animalmatchmaking.dto.FormDTO;
 import com.beartell.animalmatchmaking.service.AdopterFormService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/adopterform")
 public class AdopterFormController {
 
     @Autowired
     private AdopterFormService adopterFormService;
 
-    public boolean createForm(FormDTO formDTO) {
+    @PostMapping("/add")
+    public boolean createForm(@RequestBody FormDTO formDTO) {
         return adopterFormService.createForm(formDTO);
     }
 
+    @PatchMapping("/update")
     public boolean updateForm(FormDTO formDTO) {
         return adopterFormService.updateForm(formDTO);
     }
